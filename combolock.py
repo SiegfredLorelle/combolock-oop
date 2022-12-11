c34438f5ea4e451762a08fe8c29651914461b865
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2022-12-11 15:59:29 +0800

    2nd commit - working program, with input checking, no ui yet

--- combolock.py
+++ combolock.py
@@ -5,34 +5,68 @@ class ComboLock:
         print(f"\nInstructions: Turn right to first number code, Turn left to second number code, Turn right to third number code.\n")
 
 
-    def ComboLock(self, secret1, secret2, secret3) : 
-        self.combination = (secret1, secret2, secret3)
+    def ComboLock(self, secret1, secret2, secret3):
+        secrets = [secret1, secret2, secret3]
+        
+        for secret in secrets:
+            if not isinstance(secret, int) or secret <= 0:
+                raise ValueError("Ticks must be a positive integer")
+
+        self.combination = secrets
 
     
     def __str__(self):
         return f"{self.combination}"
 
 
-
-
-
     def reset(self):
         self.currentPosition = 0
+        self.progress = {
+            "secret1": {"isDone": False, "enteredDigit": None},
+            "secret2": {"isDone": False, "enteredDigit": None},
+            "secret3": {"isDone": False, "enteredDigit": None}
+        }
+
         print(f"Reset: currently on {self.currentPosition}")
 
     def turnLeft(self, ticks):
+        if not isinstance(ticks, int) or ticks <= 0:
+            raise ValueError("Ticks must be a positive integer")
+        else:
+            self.currentPosition = (self.currentPosition - ticks) % 40
+            print(f"Done turning left: currently on {self.currentPosition}")
 
-        self.currentPosition = (self.currentPosition - ticks) % 40
-        print(f"Done turning left: currently on {self.currentPosition}")
-    
+            if self.progress["secret1"]["isDone"]:
+                self.progress["secret2"]["isDone"] = True
+                self.progress["secret2"]["enteredDigit"] = self.currentPosition
+
+        
+
+
+
     def turnRight(self, ticks):
-        self.currentPosition = (self.currentPosition + ticks) % 40
-        print(f"Done turning right: currently on {self.currentPosition}")
+        if not isinstance(ticks, int) or ticks <= 0:
+            raise ValueError("Ticks must be a positive integer")
+        
+        else:
+            self.currentPosition = (self.currentPosition + ticks) % 40
+            print(f"Done turning right: currently on {self.currentPosition}")
 
+            if not self.progress["secret2"]["isDone"]:
+                self.progress["secret1"]["isDone"] = True
+                self.progress["secret1"]["enteredDigit"] = self.currentPosition
 
-
-
+            else:
+                self.progress["secret3"]["isDone"] = True
+                self.progress["secret3"]["enteredDigit"] = self.currentPosition
+                print("Entered 3 numbers. Try to open it.")
 
 
     def open(self):
-        ...
+        enteredDigits = [values["enteredDigit"] for key, values in self.progress.items()]
+        print(enteredDigits, self.combination) # FOR TESTING ONLY
+        if enteredDigits != self.combination:
+            print("\nLock didn't open. Reset to try again.")
+
+        else:
+            print("\nSuccessfully opened the lock!")
--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from combolock import ComboLock
 
-a = ComboLock(3, "aa" ,3)
-a.turnRight(3)
+a = ComboLock(9, 25, 2)
+a.turnRight(2)
 a.turnLeft(3)
+a.turnRight(3)
+a.open()
+
+a.reset()
+
+a.turnRight(9)
+a.turnLeft(24)
+a.turnRight(17)
+a.open()
 # a.turn
 
